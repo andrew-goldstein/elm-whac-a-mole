45e60384c801b8ad9ec1354808a054f53b83bbd8
Andrew Goldstein <abgoldst@gmail.com>
2015-10-14 20:37:22 -0600

    initial commit

---
+++ Model.elm
@@ -0,0 +1,4 @@
+module Model where
+
+type alias Wackable = Bool
+type alias Mole = { wackable : Wackable }
---
+++ View.elm
@@ -0,0 +1,9 @@
+module View where
+
+import Html            exposing (..)
+import Html.Attributes exposing (..)
+
+renderMole : Mole -> Html
+renderMole mole = if mole.wackable then
+  then img [src "http://www.fcps.edu/islandcreekes/ecology/Mammals/Eastern%20Mole/eastern1.jpg"] []
+  else img [src "http://cloud.graphicleftovers.com/20677/464110/mole-hole-in-brown-dirt-closeup.-shallow-dof.jpg"] []
