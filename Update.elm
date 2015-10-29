1c39a108afcca25cc1a1ca2903310d7b70943745
Andrew Goldstein <abgoldst@gmail.com>
2015-10-28 22:34:11 -0600

    multiple moles are rendering

--- Update.elm
+++ Update.elm
@@ -2,5 +2,16 @@ module Update where
 
 import Model exposing (..)
 
-update : () -> Mole -> Mole
-update _ m = { m | wackable <- not m.wackable }
+update : Hole -> Game -> Game
+update h g = let
+  negateMole : Mole -> Mole
+  negateMole m = { m | wackable <- not m.wackable }
+
+  moles' : List (Hole, Mole)
+  moles' = List.map (\(hole, mole) ->
+      if h == hole
+      then (hole, negateMole mole)
+      else (hole, mole)
+    ) g.moles
+
+  in { g | moles <- moles' }
