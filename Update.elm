1778fe528ecf173daa8d4b14c72ece869fc657c4
Andrew Goldstein <abgoldst@gmail.com>
2015-11-13 20:50:21 -0700

    fixed a bug where whacking a mole toggled state instead of depressing mole

--- Update.elm
+++ Update.elm
@@ -33,11 +33,10 @@ rand = let
 
   in List.map (\(a,b) -> (b,a)) <~ (fst <~ foldp f ([], R.initialSeed 0) interval)
 
+
+
 update : Input -> Game -> Game
 update input g = let
-
-  negateMole : Mole -> Mole
-  negateMole m = { m | wackable <- not m.wackable }
 
   whack : Mole -> Score
   whack m = if m.wackable then 400 else 0
@@ -45,8 +44,8 @@ update input g = let
   whacked h = List.foldl (\(hole, mole) g' ->
       if h == hole
       then { g' | score <- g'.score + whack mole
-                , moles <- (hole, negateMole mole) :: g'.moles }
-      else { g' | moles <- (hole,            mole) :: g'.moles }
+                , moles <- (hole, moleInAHole) :: g'.moles }
+      else { g' | moles <- (hole, mole)        :: g'.moles }
     ) { score = g.score, moles = [] } g.moles
 
   in case input of
